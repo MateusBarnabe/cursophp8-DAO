430f9240fcc60f35d93be0fade6d2ee1e964eeeb
Mateus Barnabé <poluxz29@gmail.com>
2024-05-27 13:26:15 -0300

    adicionamos novos metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -52,12 +52,43 @@ class usuario{
 
         }
     }
+
+    public static function getList(){
+        $sql = new SQL();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+
+    public static function search($login){
+        $sql = new SQL();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH   ORDER BY deslogin", array(
+            ":SEARCH"=>"%".$login."%"
+        ));
+    } 
+
+    public function login($login, $password){
+
+        $Sql = new SQL();
+
+        $result = $Sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :LOGIN AND dessenha LIKE :PASSWORD", array(
+            ":LOGIN"=>$login, ":PASSWORD"=>$password
+        ));
+        if (count($result)>0) {
+            $row = $result[0];
+            $this->setIdusuario($row["idusuario"]);
+            $this->setDesLogin($row["deslogin"]);
+            $this->setDesSenha($row["dessenha"]);
+            $this->setDtcadastro(new DateTime($row["dtcadastro"]));
+
+        }else {
+            throw new Exception("Login e/ou senha invalidos!");
+        }
+    }
     public function __toString()
     {
         return json_encode(array(
             "idusuario"=>$this->getIdusuario(),
-            "dessenha"=>$this->getDesSenha(),
             "deslogin"=>$this->getDesLogin(),
+            "dessenha"=>$this->getDesSenha(),
             "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y  h:i:s")
 
         ));
--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 require_once("config.php");
 
-$root = new usuario();
-
-$root->loadById(5);
-
-echo $root;
+//$root = new usuario();
+//$root->loadById(5);
 //echo $root;
+//$lista = usuario::getList();
+//echo json_encode($lista);
+
+//$search = usuario::search("ba");
+//echo json_encode($search);
+
+$cadas = new usuario();
+$cadas->login("jose", "asdf1234");
+echo $cadas;
 
 
 
